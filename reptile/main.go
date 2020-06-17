a2c29229c4fca960199244f29279ab7c6c6b68e7
v_licguo <v_licguo@tencent.com>
2020-06-17 11:07:14 +0800

    完善抓取功能

--- reptile/main.go
+++ reptile/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"myproject/models"
 	"net/http"
 	"regexp"
 	"sync"
@@ -17,8 +16,8 @@ func main() {
 
 	//http://www.doczj.com/doc/94366972524de518974b7d79-17.html
 	//http://www.doczj.com/doc/c21054447dd184254b35eefdc8d376eeaeaa1700.html
-	gogogo("http://www.doczj.com/doc/c21054447dd184254b35eefdc8d376eeaeaa1700.html")
-
+	//gogogo("http://www.doczj.com/doc/c21054447dd184254b35eefdc8d376eeaeaa1700.html")
+	gogogo("http://www.doczj.com/doc/c21054447dd184254b35eefdc8d376eeaeaa1700-2.html")
 }
 
 func gogogo(url string) {
@@ -43,26 +42,34 @@ func gogogo(url string) {
 	}
 
 	//<p>1．下面选项中，（ B ）是Java 关键字。</p><p>A、then</p><p>B、continue</p><p>C、java</p><p>D、PUBLIC</p>
-	reg := regexp.MustCompile(`<p>([0-9]+).(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p>`)
+	//<p>20.java.io包的File类是( B )</p><p>(A)字符流类(B) 字节流类 (C) 对象流类 (D) 非流类</p><p>
+	//1．使用Java语言编写的源程序保存时的文件扩展名是（ B ）。</p><p>（A）.class （B）.java （C）.cpp （D）.txt 2．设int a=-2，则表达式a>>>3的值为（ C ）。
+	//24. 若有定义：byte[] x={11,22,33,-66};</p><p>其中0≤k≤3，则对x数组元素错误的引用是（ C ）</p><p>A）x[5-3] B）x[k] C）x[k+5] D）x[0]</p
+	//reg := regexp.MustCompile(`<p>([0-9]+).(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p><p>([A-Z]+)、(?s:(.*?))</p>`)
+	reg := regexp.MustCompile(`<p>[0-9]{1,2}.\s?([\s\S]+?)</p><p>.?A.?([\s\S]+?).B.?([\s\S]+?).C.?([\s\S]+?).D.?([\s\S]+?)</p>`)
 
 	allString := reg.FindAllStringSubmatch(string(all), -1)
 	fmt.Println(len(allString))
 	for _, v := range allString {
-		redio := &models.Radio{
-			Type:    1,
-			OptionA: v[4],
-			OptionB: v[6],
-			OptionC: v[8],
-			OptionD: v[10],
-		}
-		compile := regexp.MustCompile(`(.*（ ?)([A-Z])( ?）.*)`)
-		submatch := compile.FindAllStringSubmatch(v[2], -1)
-		replaceAllString := compile.ReplaceAllString(v[2], "$1$3")
-		redio.Title = replaceAllString
+		fmt.Printf("%s A:%s  B:%s  C:%s  D:%s\n", v[1], v[2], v[3], v[4], v[5])
+		//redio := &models.Radio{
+		//	Type:    1,
+		//	OptionA: v[4],
+		//	OptionB: v[6],
+		//	OptionC: v[8],
+		//	OptionD: v[10],
+		//}
+		compile := regexp.MustCompile(`([\s\S]+?(\(|（).?)([A-Z])([\s\S]+)`)
+		submatch := compile.FindAllStringSubmatch(v[1], -1)
+		replaceAllString := compile.ReplaceAllString(v[1], "$1$4")
+		fmt.Printf("replaceAllString = %s\n", replaceAllString)
+		//redio.Title = replaceAllString
+		//fmt.Printf("A:%s  B:%s  C:%s  D:%s\n",v[4],v[6],v[8],v[10])
 		for _, c := range submatch {
-			redio.Answer = c[2]
+			//redio.Answer = c[2]
+			fmt.Println(c[3])
 		}
-		models.SaveRadio(redio)
+		//models.SaveRadio(redio)
 	}
 	urlMap.Store(url, 1)
 
